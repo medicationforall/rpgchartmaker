5741fe304276e97556b56d373faa0931a2cc48b9
ADAMS James <james@medicationforall.com>
2017-06-12 12:49:36 -0400

    Split Out util code.

--- script/main.js
+++ script/main.js
@@ -15,7 +15,6 @@
  *   You should have received a copy of the GNU Lesser General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 $.fn.extend({
     animateCss: function (animationName) {
         var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -25,97 +24,10 @@ $.fn.extend({
     }
 });
 
-
 $(document).ready(function(){
+  Util.call(this);
 
-	//d40444 Demonkin d5 d6+1 d6/2 d4*10 d6-1
-	//should have 6 matches d40444, d5, d6+1, d6/2, d4*10, and d6-1
-	var findDice=function(text){
-		var re = /\bd(\d+)([*+-/%]?)(\d+)?\b/gi;
-
-		text = text.replace(re,function(match,number, operation, number2){
-			var n = parseInt(number);
-			var roll = Math.floor(Math.random() * n);
-			//normalize roll so that it's not 0 index
-			roll++;
-
-			//modify roll
-			if(operation && number2 && operation !==''){
-				var n2 = parseInt(number2);
-
-				if(operation=='+'){
-					roll = roll+n2;
-				}else if(operation=='-'){
-					roll = roll-n2;
-				}else if(operation=='/'){
-					roll = roll/n2;
-				}else if(operation=='*'){
-					roll = roll*n2;
-				}else if(operation=='%'){
-					roll = roll%n2;
-				}
-			}
-			return roll;
-		});
-		return text;
-	};
-
-
-	//d6 [Region](1,5) Orks - [Type]
-	//Hits on Type, and Region subset 1, and 5.
-	var findList=function(text){
-		var re = /\[(.*?)\]\(?([\d,]*)\)?/gi
-
-		text = text.replace(re,function(match,listName,qualifier){
-			var returner = match;
-			if(listName && listName!==''){
-				var list = $('.list input[name="listGroupName"]').filter(function(){return this.value==listName;}).closest('.list');
-				return rollList(list,true,qualifier);
-			}
-
-			return returner;
-		});
-		return text;
-	}
-
-
-	/**
- 	 * Potentially a recursive call, depending on how the user structured their data.
-	 */
-	var rollList=function(list,forceRoll,qualifier){
-		//make sure it's not skipped
-		if(forceRoll || $(list).find('input[name="roll"]').prop('checked')){
-
-			var arr = [];
-
-			if(qualifier && qualifier!==''){
-				var sub  = qualifier.split(',');
-
-				for(var i=0;i<sub.length;i++){
-					var node = $(list).find('ol li:nth-child('+sub[i].trim()+')');
-					arr.push(node.html());
-				}
-			}else{
-				//place list item contents into an array
-				arr = $(list).find('ol li').map(function(i, el) {
-					return $(el).html();
-				}).get();
-			}
-
-			var roll = Math.floor(Math.random() * arr.length);
-			var value = arr[roll];
-			//lookup for dice
-			value = findList(value);
-			value = findDice(value);
-
-			//animate the roll selection
-			$(list).find('ol li:nth-child('+(roll+1)+')').animateCss('highlight');
-
-			return value;
-		}
-	}
-
-/*MAIN*/
+  /*MAIN*/
 	$('.javacriptWarning').remove();
 
 	//sortable for lists in groupcontainer
@@ -139,7 +51,7 @@ $(document).ready(function(){
 
 
 	//roll button click
-	$('.rollButton').click(function(event){
+	$('.rollButton').click($.proxy(function(event){
 		event.preventDefault();
 
 		//change the rollContainers titles color
@@ -149,7 +61,6 @@ $(document).ready(function(){
 		var rollTable = $('.rollContainer table');
 		$(rollTable).find('th').remove();
 		$(rollTable).find('tbody tr').remove();
-
 
 		//fill out headers
 		$(rollTable).find('thead').append('<th>'+'No.'+'</th>');
@@ -173,15 +84,18 @@ $(document).ready(function(){
 			//add roll Index
 			$('.rollContainer table tbody tr:last-child').append('<td>'+(i+1)+'.'+'</td>');
 
-			var list =$('.list').each(function(index, item){
-				rollValue = rollList(item);
-
-				if(rollValue!==undefined){
-					$('.rollContainer table tbody tr:last-child').append('<td>'+rollValue+'</td>');
-				}
-			});
+			var list =$('.list').each($.proxy(this.getRollValue,this));
 		}
-	});
+	},this));
+  
+
+  this.getRollValue=function(index, item){
+    rollValue = this.rollList(item);
+
+    if(rollValue!==undefined){
+      $('.rollContainer table tbody tr:last-child').append('<td>'+rollValue+'</td>');
+    }
+  };
 
 
 	//main
